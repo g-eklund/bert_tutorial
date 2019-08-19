b68d843fcca3ba1c933a7476a9450cc6d608fb3a
g-eklund <gustav.eklund@protonmail.com>
2019-08-19 23:18:50 +0200

    Update bert_call.py

--- bert_call.py
+++ bert_call.py
@@ -1,8 +1,12 @@
 
 
 from bert-serving.client import BertClient
+import numpy as np
 
 bc = BertClient()
 strings = ['hej hopp', 'hej hopp']
 enc = bc.encode(strings)
 
+
+def cosine_sim(s1, s2):
+    return np.dot(s1[0,:],s2[0,:])/(np.linalg.norm(s1)*np.linalg.norm(s2)+1e-9)
